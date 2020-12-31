1f1abfb2a343afa9ca04745311814fc444aded0c
ziyun99 <42372385+ziyun99@users.noreply.github.com>
2020-12-31 16:58:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,46 @@ import sys
 import os
 import importlib
 
+attack_methods = ["fooling", "invisible", "seeing", "shapeshifter"]
+
 def create_attacker(argvs):
     # get params
+
+    if len(argvs) == 2:
+        input_method = argvs[1]
+        if input_method not in attack_methods:
+            print("Usage: python3 main.py [attack_method] [attack_config]")
+            print("Attack method '{}' not found, possible attack methods are {}".format(input_method, str(attack_methods)))
+            exit()
+        cfgfile = importlib.import_module("configs." + input_method)
+        print("Usage: python3 main.py [attack_method] [attack_config]") 
+        print("Possible configurations for '{}' are: {}, pls refer to 'configs/{}.py' for more details".format(input_method, list(cfgfile.custom_configs.keys()), input_method))
+        exit()
+        
     if len(argvs) < 3:
         print("Usage: python3 main.py [attack_method] [attack_config]")
+        print("Possible attack methods are {}".format(str(attack_methods)))
         print("eg. python3 main.py fooling paper_obj")
         exit()
+        
+        
     input_method = argvs[1]
     input_config = argvs[2]
     input_args = argvs[3:]
+    
+    if input_method not in attack_methods:
+        print("Attack method '{}' not found, possible attack methods are {}".format(input_method, str(attack_methods)))    
 
     # add to PYTHONPATH
     attackpath = os.path.join(os.getcwd(),input_method) 
     sys.path.insert(0, attackpath)
-
+    
     # get config
     try:
         cfgfile = importlib.import_module("configs." + input_method)
         config = cfgfile.custom_configs[input_config]()
-    # except ModuleNotFoundError as e:
-    #     print(e)
-    #     exit()
     except KeyError:
-        print("Configuration '{}' not found, pls refer to configs/{}".format(input_config, input_method))
+        print("Configuration '{}' not found, pls refer to configs/{}.py".format(input_config, input_method))
         print("Possible configurations for '{}' are: {}".format(input_method, list(cfgfile.custom_configs.keys())))
         exit()
         
